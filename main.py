aef1929570196ebe0436b96ca7b0d72c32445ffd
Kirill Gmirya <Gmiryk_Kirill@mail.ru>
2026-01-28 19:28:34 +0300

    phone numbers are fixed

--- main.py
+++ main.py
@@ -37,12 +37,25 @@ for contact in contacts_list[1:]:
     if len(fio_components) >= 3:
         contact[2] = fio_components[2]
 
+# pprint(contacts_list)
+
+# Заменяем нгомера телефонов по шаблону +7(999)999-99-99 доб.9999
+
+# pattern = r"(\+7|8)?\s*\(?(\d{3})\)?[\s\-]*(\d{3})[\s\-]*(\d{2})[\s\-]*(\d{2})(?:\s*\(?(доб\.?)\s*(\d+)\)?)?"
+# replacement = r"+7(\2)\3-\4-\5 \6\7"  # правильно, но оставляет пробел после номера при отсутствии добавочного
+
+
+for row in contacts_list[1:]:
+    # row[5] = re.sub(pattern, replacement, row[5])
+
+    pattern = r"(\+7|8)?\s*\(?(\d{3})\)?[\s\-]*(\d{3})[\s\-]*(\d{2})[\s\-]*(\d{2})"
+    replacement = r"+7(\2)\3-\4-\5"
+    row[5] = re.sub(pattern, replacement, row[5])
+
+    # Вторая проходка только для добавочного
+    pattern_2 = r"\s*\(?(доб\.?)\s*(\d+)\)?"
+    replacement_2 = r" доб.\2"
+    row[5] = re.sub(pattern_2, replacement_2, row[5])
+
+    # print(row[5])
 pprint(contacts_list)
-
-
-# # TODO 2: сохраните получившиеся данные в другой файл
-# # код для записи файла в формате CSV
-# with open("phonebook.csv", "w", encoding="utf-8") as f:
-#     datawriter = csv.writer(f, delimiter=",")
-#     # Вместо contacts_list подставьте свой список
-#     datawriter.writerows(contacts_list)
